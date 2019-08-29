b93188f90e33a7a6083a6c0c9258aec9df7938b3
Esa Firman <esafirm21@gmail.com>
2019-08-29 07:58:53 +0700

    Complete

--- src/main/kotlin/GraphQLBuilder.kt
+++ src/main/kotlin/GraphQLBuilder.kt
@@ -1,3 +1,42 @@
-fun main() {
+typealias Builder = GraphQLBuilder.() -> Unit
 
+class GraphQLBuilder(val name: String) {
+    val childs = mutableListOf<GraphQLBuilder>()
+
+    override fun toString(): String {
+        val parent = nodeAsString()
+        val child = childs.fold("") { acc, graphQLBuilder ->
+            "$acc\n$graphQLBuilder"
+        }
+        return parent.format(child)
+    }
+
+    private fun nodeAsString(): String {
+        if (childs.isNotEmpty()) {
+            return "{$name\n%s}"
+        }
+        return name
+    }
+}
+
+fun GraphQLBuilder.query(name: String, block: GraphQLBuilder.() -> Unit = {}): GraphQLBuilder {
+    return GraphQLBuilder(name).apply(block).also {
+        this.childs.add(it)
+    }
+}
+
+internal operator fun String.invoke(block: GraphQLBuilder.() -> Unit): GraphQLBuilder {
+    return GraphQLBuilder(this).apply(block)
+}
+
+fun main() {
+    val builder = "users" {
+        query("name")
+        query("address") {
+            query("city")
+            query("postCode")
+        }
+    }
+
+    println(builder.toString())
 }
