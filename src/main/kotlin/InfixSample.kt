bcf2738e1900b4b7be36bbe581a0722e47b6cab6
Esa Firman <esafirm21@gmail.com>
2019-09-16 10:51:53 +0700

    edited

--- src/main/kotlin/InfixSample.kt
+++ src/main/kotlin/InfixSample.kt
@@ -1,19 +1,19 @@
 infix fun Int.tambah(other: Int) = println(this + other)
 infix fun Int.kurang(other: Int) = println(this - other)
 
-class JikaWrapper {
+class ConditionWrapper {
     var firstValue: Any? = null
     var secondValue: Any? = null
 }
 
-infix fun Unit.jika(condition: Any): JikaWrapper {
+infix fun Unit.`if`(condition: Any): ConditionWrapper {
     print("Jika value pertama ")
-    return JikaWrapper().apply {
+    return ConditionWrapper().apply {
         firstValue = condition
     }
 }
 
-infix fun JikaWrapper.adalah(condition: Any): JikaWrapper {
+infix fun ConditionWrapper.`is`(condition: Any): ConditionWrapper {
     print("adalah $condition ")
     return if (secondValue == null) {
         apply {
@@ -25,16 +25,16 @@ infix fun JikaWrapper.adalah(condition: Any): JikaWrapper {
         }
 
         if (firstValue == true && secondValue == true) {
-            println("\nKondisi kamu sudah benar 👍")
+            println("\nYour condition is right 👍")
         } else {
-            println("\nKondisi kamu masih salah 🙈")
+            println("\nYour condition is wrong 🙈")
         }
 
         this
     }
 }
 
-infix fun JikaWrapper.maka(condition: Any): JikaWrapper {
+infix fun ConditionWrapper.then(condition: Any): ConditionWrapper {
     print("maka value kedua ")
     return apply { secondValue = condition }
 }
@@ -45,5 +45,5 @@ fun main() {
     1 tambah 1
     2 kurang 1
 
-    `👉` jika 3 * 10 adalah 30 maka 2 * 10 adalah 20
+    `👉` `if` 3 * 10 `is` 30 then 2 * 10 `is` 20
 }
